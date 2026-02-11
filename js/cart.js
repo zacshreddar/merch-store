fa80c14a87a4478becfe378e4eff8327bcb56830
zacshreddar <njorogeian48@gmail.com>
2026-02-11 11:30:02 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,12 @@
+function addToCart(id) {
+  const size = document.getElementById(`size-${id}`).value;
+  const color = document.getElementById(`color-${id}`).value;
+  const product = products.find(p => p.id === id);
+
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push({ ...product, size, color, qty: 1 });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to cart ✔️");
+}
 
