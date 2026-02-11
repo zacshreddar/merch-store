8ab5cc9960c8a1c09e5469f25d5b524052ee4e91
zacshreddar <njorogeian48@gmail.com>
2026-02-11 11:32:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+const CACHE = "merch-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/cart.html",
+  "/checkout.html",
+  "/css/style.css",
+  "/js/products.js",
+  "/js/cart.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
 
